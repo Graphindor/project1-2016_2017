5bdebf37a542bc27ac2b25a9186c561beab4389a
Gilmo <agilmozzi@gmail.com>
2016-11-18 10:46:33 +0100

    Best time - memory so far

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
@@ -7,57 +6,153 @@
 
 using namespace std;
 
-struct nodo {
+struct node {
 	vector<int> neighbor;
 	bool visited = false;
 };
 
-typedef vector<nodo> grafo_t;
+typedef vector<node> graph_t;
+typedef pair<int, int> edge_t;
+typedef vector<int> path_t;
 
-typedef vector<vector<int>> species_t;
+void dfsExplore(graph_t& graph, int u, path_t& visitOrder, vector<path_t>& cyclesList);
+vector<path_t> getCycles(graph_t& graph);
+unsigned int countCommonEdges(path_t path1, path_t path2);
+unsigned int mcd(unsigned int x, unsigned int y);
 
-// Le sposteremo
-void DFS(grafo_t& GRAFO, int u, vector<int>& ordineVisita, vector<vector<int>>& listaCicli)
+int main()
 {
-	GRAFO[u].visited = true;
-	for(int k:GRAFO[u].neighbor)
+	int N, E;
+
+	ifstream in("input.txt");
+	in >> N >> E;
+
+	graph_t graph(N);
+	vector<edge_t> edges;
+
+	for(int i = 0; i < E; i++)
 	{
-		if(!GRAFO[k].visited && !(GRAFO[k].neighbor.empty()))
+		int source, destination;
+		in >> source >> destination;
+
+		edge_t edge(source, destination);
+		edges.push_back(edge);
+
+		graph[source].neighbor.push_back(destination);
+		graph[destination].neighbor.push_back(source);
+	}
+
+	vector<path_t> cycleList = getCycles(graph);
+
+	unsigned int maxSpecies = cycleList[0].size();
+	for(size_t i = 0; i < cycleList.size() - 1; i++)
+	{
+		unsigned int _maxSpecies = maxSpecies;
+		maxSpecies = mcd(cycleList[i].size(), cycleList[i + 1].size());
+		maxSpecies = mcd(maxSpecies, _maxSpecies);
+
+		unsigned int commonEdgesCount =
+			countCommonEdges(cycleList[i], cycleList[i + 1]);
+
+		maxSpecies = mcd(
+			maxSpecies,
+			cycleList[i].size() + cycleList[i + 1].size() - commonEdgesCount
+		);
+	}
+
+	vector<int> species(E);
+
+	for(path_t cycle:cycleList)
+	{
+		int currentSpecie = 0;
+		cycle.push_back(cycle[0]);
+		for(size_t i = 0; i < cycle.size() - 1; i++)
 		{
-			ordineVisita.push_back(k);
-			DFS(GRAFO, k, ordineVisita, listaCicli);
-			ordineVisita.pop_back();
+			currentSpecie %= maxSpecies;
+
+			int n1, n2;
+			n1 = cycle[i];
+			n2 = cycle[i + 1];
+
+			if(n1 > n2)
+			{
+				swap(n1, n2);
+			}
+
+			pair<int, int> edge = make_pair(n1, n2);
+			unsigned int pos = find(edges.begin(), edges.end(), edge) - edges.begin();
+			species[pos] = currentSpecie;
+
+			currentSpecie++;
+		}
+	}
+
+	ofstream out("output.txt");
+
+	out << maxSpecies << endl;
+
+	for(int specie:species)
+		out << specie << endl;
+
+	out.close();
+
+	return 0;
+}
+
+void dfsExplore(graph_t& graph, int u, vector<int>& visitOrder, vector<vector<int>>& cyclesList)
+{
+	graph[u].visited = true;
+	for(int node:graph[u].neighbor)
+		if(!graph[node].visited && !(graph[node].neighbor.empty()))
+		{
+			visitOrder.push_back(node);
+			dfsExplore(graph, node, visitOrder, cyclesList);
+			visitOrder.pop_back();
 		}
 		else
 		{
-			if(k != ordineVisita[ordineVisita.size() - 2])
+			if(node != visitOrder[visitOrder.size() - 2])
 			{
-				unsigned int pos = find(ordineVisita.begin(), ordineVisita.end(), k) - ordineVisita.begin();
-				vector<int> toPush(ordineVisita.begin() + pos, ordineVisita.end());
+				unsigned int pos = find(
+					visitOrder.begin(),
+					visitOrder.end(),
+					node) - visitOrder.begin();
+
+				path_t toPush(visitOrder.begin() + pos, visitOrder.end());
 
 				if(!toPush.empty())
-					listaCicli.push_back(toPush);
+					cyclesList.push_back(toPush);
 			}
 		}
-	}
 }
 
-vector<vector<int>> getCycles(grafo_t& GRAFO)
+vector<path_t> getCycles(graph_t& graph)
 {
-	vector<vector<int>> listaCicli;
-	vector<int> ordineVisita;
+	vector<path_t> cyclesList;
+	path_t visitOrder;
 
-	for(size_t i = 0; i < GRAFO.size(); i++)
-		if(!GRAFO[i].visited)
+	for(size_t node = 0; node < graph.size(); node++)
+		if(!graph[node].visited)
 		{
-			ordineVisita.push_back(i);
-			DFS(GRAFO, i, ordineVisita, listaCicli);
+			visitOrder.push_back(node);
+			dfsExplore(graph, node, visitOrder, cyclesList);
 		}
 
-	return listaCicli;
+	return cyclesList;
 }
 
-int MCD(unsigned int x, unsigned int y)
+unsigned int countCommonEdges(path_t path1, path_t path2)
+{
+	unsigned int count = 0;
+	for(int n1:path1)
+		for(int n2:path2)
+			if(n1 == n2)
+				count++;
+
+	return (count > 0) ? count - 1 : 0;
+}
+
+unsigned int mcd(unsigned int x, unsigned int y)
 {
 	while(y != 0)
 	{
@@ -67,152 +162,4 @@ int MCD(unsigned int x, unsigned int y)
 	}
 
 	return x;
-}
-
-int main()
-{
-
-	ifstream in("input.txt");
-
-	int M, L;
-	in >> M >> L;
-
-	grafo_t GRAFO(M);
-	species_t species(M);
-	vector<pair<int, int>> edges;
-
-
-	for(int i = 0; i < M; i++)
-		species[i].resize(M);
-
-	for(int i = 0; i < L; i++)
-	{
-		int src, dest;
-		in >> src >> dest;
-
-		pair<int, int> edge(src, dest);
-
-		edges.push_back(edge);
-		GRAFO[src].neighbor.push_back(dest);
-		GRAFO[dest].neighbor.push_back(src);
-	}
-
-	cout << endl << "=== CHECK GRAFO ===" << endl;
-	for(unsigned int i = 0; i < GRAFO.size(); i++)
-	{
-		cout << i << ": ";
-		for(int j:GRAFO[i].neighbor)
-			cout << "[" << j << ", " << species[i][j] << "]";
-
-		cout << endl;
-	}
-
-	cout << endl << "=== FIND CYCLES===" << endl;
-	vector<vector<int>> cycleList = getCycles(GRAFO);
-
-	int x = 1;
-	for(vector<int> v:cycleList)
-	{
-		cout << "[" << x++ << "] ";
-		for(int i:v)
-			cout << i << " ";
-
-		cout << endl;
-	}
-
-	// unsigned int mcd = cycleList[0].size();
-	// unsigned int min = M;
-
-	// for(vector<int> v:cycleList)
-	// {
-	// 	if(min > v.size())
-	// 		min = v.size();
-	//
-	// 	int x = mcd;
-	// 	int y = v.size();
-	//
-	// 	// Il MCD e` calcolato usando l'algoritmo Euclideo
-	// 	while (y > 0) {
-	// 		int r = x % y;
-	// 		x = y;
-	// 		y = r;
-	// 	}
-	//
-	// 	mcd = x;
-	// }
-
-	unsigned int mcd = cycleList[0].size();
-	for(unsigned int i = 0; i < cycleList.size() - 1; i++)
-	{
-		unsigned int _mcd = mcd;
-		mcd = MCD(cycleList[i].size(), cycleList[i + 1].size());
-		mcd = MCD(mcd, _mcd);
-
-		unsigned int common_arch = 0;
-		for(int z:cycleList[i])
-		{
-			for(int j:cycleList[i + 1])
-			{
-				if(z == j)
-					common_arch++;
-			}
-		}
-		common_arch = (common_arch > 0) ? common_arch - 1 : 0;
-
-		cout << common_arch << endl;
-
-		mcd = MCD(mcd, cycleList[i].size() + cycleList[i + 1].size() - common_arch);
-	}
-
-	cout << "MCD: " << mcd << endl;
-	vector<int> MCDS;
-	for(unsigned int i = 1; i <= (mcd / 2); i++)
-		if((mcd % i) == 0)
-			MCDS.push_back(i);
-
-	MCDS.push_back(mcd);
-	reverse(MCDS.begin(), MCDS.end());
-
-	cout << "MCDS: ";
-	for(int m:MCDS)
-		cout << m << " ";
-	cout << endl;
-
-	for(vector<int> cycle:cycleList)
-	{
-		int current = 0;
-		cycle.push_back(cycle[0]);
-		for(unsigned int i = 0; i < cycle.size() - 1; i++)
-		{
-
-			current %= mcd;
-
-			int n1, n2;
-			n1 = cycle[i];
-			n2 = cycle[i + 1];
-
-			int* sp1 = &(species[n1][n2]);
-			int* sp2 = &(species[n2][n1]);
-
-			*sp1 = current + 1;
-			*sp2 = current + 1;
-			current++;
-		}
-	}
-
-	cout << endl << "=== CHECK SPECIES ===" << endl;
-
-	ofstream out("output.txt");
-
-	out << mcd << endl;
-	for(pair<int, int> edge:edges)
-	{
-		int spec = species[edge.first][edge.second] - 1;
-		cout << edge.first << ", " << edge.second << " - " << spec << endl;
-		out << ((spec < 0) ? 0 : spec) << endl;
-	}
-
-	out.close();
-
-	return 0;
 }
